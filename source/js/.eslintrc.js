e69e0b2cdc3a93c2b9d6137a21e7aa836af9d290
Morilence <1280659615@qq.com>
2022-09-16 20:01:10 +0800

    chore: changed local ESLint configuration in source/js

---
+++ source/js/.eslintrc.js
@@ -0,0 +1,11 @@
+/**
+ * rules: https://eslint.bootcss.com/docs/rules
+ */
+module.exports = {
+    extends: "../../.eslintrc.js",
+    env: {
+        browser: true,
+        jquery: true,
+        node: true,
+    },
+};
